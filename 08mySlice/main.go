fd206c3113d88a31918a1e30b23c4725aa714b36
Eccentric19Shivam <singhshivamhp16@gmail.com>
2024-08-16 20:10:36 +0530

    ye

--- 08mySlice/main.go
+++ 08mySlice/main.go
@@ -2,19 +2,20 @@ package main
 
 import (
 	"fmt"
+	// "sort"
 )
 
 func main() {
 	fmt.Println("Welcome to Slices!")
 
-	// var fruitList = []string{"Apple", "Tomato", "Peach"}
-	// fmt.Printf("Type of fruit list %T \n", fruitList)
+	var fruitList = []string{"Apple", "Tomato", "Peach"}
+	fmt.Printf("Type of fruit list %T \n", fruitList)
 
-	// fruitList = append(fruitList, "Mango", "Banana")
-	// fmt.Println(fruitList)
+	fruitList = append(fruitList, "Mango", "Banana")
+	fmt.Println(fruitList)
 
-	// fruitList = append(fruitList[:3]) //last range is alway non-inclusive
-	// fmt.Println(fruitList)
+	fruitList = append(fruitList[:3]) //last range is alway non-inclusive
+	fmt.Println(fruitList)
 
 	// highScore := make([]int, 4)
 
@@ -34,12 +35,12 @@ func main() {
 	// fmt.Println(highScore)
 	// fmt.Println(sort.IntsAreSorted((highScore)))
 
-	// How to remove a value from slices based on index
+	// // How to remove a value from slices based on index
 
-	var courses = []string{"reactjs", "javascript", "swift", "python", "ruby"}
-	fmt.Println(courses)
-	var index int = 2
-	courses = append(courses[:index], courses[index+1:]...)
-	fmt.Println(courses)
+	// var courses = []string{"reactjs", "javascript", "swift", "python", "ruby"}
+	// fmt.Println(courses)
+	// var index int = 2
+	// courses = append(courses[:index], courses[index+1:]...)
+	// fmt.Println(courses)
 
 }
